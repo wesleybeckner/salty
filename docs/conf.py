4deeef6d25189088ad6900a2c2bc41b035539e16
wesleybeckner <wesleybeckner@gmail.com>
2018-03-25 09:45:32 -0700

    transfering over plumitas docs style

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- General configuration ------------------------------------------------
@@ -33,8 +33,9 @@ sys.path.insert(0, os.path.abspath('.'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages',
     'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
