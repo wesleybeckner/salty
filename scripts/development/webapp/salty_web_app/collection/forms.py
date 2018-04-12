dbaff5c3bc99a71c877f1b3af7a2e2b1b45d729b
wesleybeckner <wesleybeckner@gmail.com>
2018-04-12 07:16:20 -0700

    updated cache based on .gitignore

--- scripts/development/webapp/salty_web_app/collection/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-
-from collection.models import StatsModels
-
-class StatsModelsForm(ModelForm):
-    class Meta: 
-        model = StatsModels
-        fields = ('name', 'description',)
--- scripts/development/webapp/salty_web_app/collection/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class StatsModels(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    slug = models.SlugField(unique=True)
